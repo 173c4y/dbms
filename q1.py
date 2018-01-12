d124b499999c7b43c3183b18c932831370966502
RIYA SINHA <riya.sinha1998@gmail.com>
2018-01-12 21:06:15 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,34 @@
+arr=[]
+fp = open(r"C:\\Users\\riyas\\Desktop\\db_q1.txt.txt","r")
+
+lines = fp.readlines()
+print(lines)
+for line in lines:
+    "".join(line)
+    line = line.split()
+    arr.append([line[0],line[1],line[2]])
+ 	
+fp.close()
+print(arr)
+
+field = input("Enter field to be added : ")
+
+val =0
+if(field=="ID"):
+    for i in range(0,len(arr)):
+        #for j in i:
+        k = int(arr[i][0])
+        print(k)
+        val = val+k
+        
+    print(val)
+elif(field=="Status"):
+    print("Error:Cannot Sum String Values")
+elif(field=="Price"):
+    for i in range(0,len(arr)):
+        k = float(arr[i][2])
+        val = val+k
+    print(val)
+    
+    
+
